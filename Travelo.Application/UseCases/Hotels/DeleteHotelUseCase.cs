bd7e9b3911e5cb33a8a93d9b51ad0b873262fdb0
Eman <emmy20029@gmail.com>
2026-02-09 09:48:20 +0200

    Last Version

--- Travelo.Application/UseCases/Hotels/DeleteHotelUseCase.cs
+++ Travelo.Application/UseCases/Hotels/DeleteHotelUseCase.cs
@@ -8,7 +8,7 @@ namespace Travelo.Application.UseCases.Hotels
         private readonly IUnitOfWork _unitOfWork;
         public DeleteHotelUseCase (IUnitOfWork unitOfWork) => _unitOfWork=unitOfWork;
 
-        public async Task<GenericResponse<string>> ExecuteAsync (int id, string user)
+        public async Task<GenericResponse<string>> ExecuteAsync (int id)
         {
             return await _unitOfWork.Hotels.DeleteHotelAsync(id);
         }
